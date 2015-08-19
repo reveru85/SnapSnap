ac0f1e8f5348f54af748b7855b628041ca21e202
Benjamin <reveru@gmail.com>
2015-08-19 23:04:57 +0800

    fixed initial entry touchable bug

--- SnapSnap/SnapSnap/PostTableViewCell.swift
+++ SnapSnap/SnapSnap/PostTableViewCell.swift
@@ -35,93 +35,11 @@ class PostTableViewCell: UITableViewCell {
     
     @IBAction func PostLikeButtonTouch(sender: UIButton) {
         
-        if parentView is HomeViewController {
+        if PostId != nil {
+            if parentView is HomeViewController {
                 
-            // Like post API
-            var urlString = "http://0720backendapi15.snapsnap.com.sg/index.php/dphodto/action_like/" + PostId! + "/" + UserId!
-            let url = NSURL(string: urlString)
-            var request = NSURLRequest(URL: url!)
-            let queue: NSOperationQueue = NSOperationQueue.mainQueue()
-            NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-                
-                if error == nil {
-                    if (response as! NSHTTPURLResponse).statusCode == 200 {
-                        if data != nil {
-                            var str = NSString(data: data, encoding: NSUTF8StringEncoding)
-                            
-                            if str == "completed" {
-                                // Update immediate UI
-                                self.PostLikeButton.imageView?.image = UIImage(named:"ic_like_on")
-                                self.IsLike = true;
-                                
-                                // Update post entry variable in HomeViewController (backend data)
-                                (self.parentView as! HomeViewController).data.likePost(self.PostId!)
-                                
-                                // Update post cell display in HomeViewController (frontend display)
-                                var likesInt = self.PostLikeCount.text?.toInt()
-                                likesInt!++
-                                self.PostLikeCount.text = String(likesInt!)
-                            }
-                            else if str == "liked" {
-                                self.PostLikeButton.imageView?.image = UIImage(named:"ic_like_on")
-                                
-                                var likeAlert = UIAlertController(title: "", message: "You have liked the post.", preferredStyle: UIAlertControllerStyle.Alert)
-                                likeAlert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Cancel, handler: nil))
-                                (self.parentView as! HomeViewController).presentViewController(likeAlert, animated: true, completion: nil)
-                            }
-                        }
-                    } else {
-                        println(response)
-                        // Insert action here for updating UI
-                    }
-                } else {
-                    println(error)
-                    // Insert action here for updating UI
-                }
-            })
-        }
-    }
-    
-    @IBAction func PostCommentButtonTouch(sender: UIButton) {
-        
-        if parentView is HomeViewController {
-            (parentView as! HomeViewController).selectedPostCellId = PostId
-            (parentView as! HomeViewController).selectedPostCell = self
-        }
-        
-        parentView.performSegueWithIdentifier("ShowComments", sender:self)
-    }
-    
-    @IBAction func MoreButtonTouch(sender: UIButton) {
-        
-        let optionMenu = UIAlertController(title: "More options", message: nil, preferredStyle: .Alert)
-        
-        let shareAction = UIAlertAction(title: "Share on Facebook", style: .Default, handler: {
-            (alert: UIAlertAction!) -> Void in
-            
-            if self.parentView is HomeViewController {
-                
-                let content : FBSDKShareLinkContent = FBSDKShareLinkContent()
-                content.contentURL = NSURL(string: "http://www.snapsnap.com.sg/webdev/album")// + self.PostId)
-                content.contentTitle = self.PostTitle.text
-                content.contentDescription = self.PostHashtags.text
-                content.imageURL = NSURL(string: (self.parentView as! HomeViewController).data.findEntry(self.PostId).media_url!)
-                
-                let button : FBSDKShareButton = FBSDKShareButton()
-                button.shareContent = content
-                button.frame = CGRectMake((UIScreen.mainScreen().bounds.width - 100) * 0.5, 50, 100, 25)
-                button.hidden = true
-                button.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
-                
-            }
-        })
-
-        let flagAction = UIAlertAction(title: "Flag as inappropriate", style: .Destructive, handler: {
-            (alert: UIAlertAction!) -> Void in
-            
-            if self.parentView is HomeViewController {
-                
-                var urlString = "http://0720backendapi15.snapsnap.com.sg/index.php/dphodto/action_flag_as_inappropriate/" + self.PostId
+                // Like post API
+                var urlString = "http://0720backendapi15.snapsnap.com.sg/index.php/dphodto/action_like/" + PostId! + "/" + UserId!
                 let url = NSURL(string: urlString)
                 var request = NSURLRequest(URL: url!)
                 let queue: NSOperationQueue = NSOperationQueue.mainQueue()
@@ -133,13 +51,24 @@ class PostTableViewCell: UITableViewCell {
                                 var str = NSString(data: data, encoding: NSUTF8StringEncoding)
                                 
                                 if str == "completed" {
-                                    // Remove post from post data in code behind and refresh view
-                                    (self.parentView as! HomeViewController).data.removeEntry(self.PostId)
-                                    (self.parentView as! HomeViewController).HomeTableView.reloadData()
+                                    // Update immediate UI
+                                    self.PostLikeButton.imageView?.image = UIImage(named:"ic_like_on")
+                                    self.IsLike = true;
                                     
-                                    var flagAlert = UIAlertController(title: "", message: "You have flagged the post as inappropriate.", preferredStyle: UIAlertControllerStyle.Alert)
-                                    flagAlert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Cancel, handler: nil))
-                                    (self.parentView as! HomeViewController).presentViewController(flagAlert, animated: true, completion: nil)
+                                    // Update post entry variable in HomeViewController (backend data)
+                                    (self.parentView as! HomeViewController).data.likePost(self.PostId!)
+                                    
+                                    // Update post cell display in HomeViewController (frontend display)
+                                    var likesInt = self.PostLikeCount.text?.toInt()
+                                    likesInt!++
+                                    self.PostLikeCount.text = String(likesInt!)
+                                }
+                                else if str == "liked" {
+                                    self.PostLikeButton.imageView?.image = UIImage(named:"ic_like_on")
+                                    
+                                    var likeAlert = UIAlertController(title: "", message: "You have liked the post.", preferredStyle: UIAlertControllerStyle.Alert)
+                                    likeAlert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Cancel, handler: nil))
+                                    (self.parentView as! HomeViewController).presentViewController(likeAlert, animated: true, completion: nil)
                                 }
                             }
                         } else {
@@ -152,25 +81,106 @@ class PostTableViewCell: UITableViewCell {
                     }
                 })
             }
-        })
+        }
+    }
+    
+    @IBAction func PostCommentButtonTouch(sender: UIButton) {
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {
-            (alert: UIAlertAction!) -> Void in
-        })
-
-        optionMenu.addAction(shareAction)
-        optionMenu.addAction(flagAction)
-        optionMenu.addAction(cancelAction)
+        if PostId != nil {
+            if parentView is HomeViewController {
+                (parentView as! HomeViewController).selectedPostCellId = PostId
+                (parentView as! HomeViewController).selectedPostCell = self
+            }
+            
+            parentView.performSegueWithIdentifier("ShowComments", sender:self)
+        }
+    }
+    
+    @IBAction func MoreButtonTouch(sender: UIButton) {
         
-        parentView.presentViewController(optionMenu, animated: true, completion: nil)
+        if PostId != nil {
+            
+            let optionMenu = UIAlertController(title: "More options", message: nil, preferredStyle: .Alert)
+            
+            let shareAction = UIAlertAction(title: "Share on Facebook", style: .Default, handler: {
+                (alert: UIAlertAction!) -> Void in
+                
+                if self.parentView is HomeViewController {
+                    
+                    let content : FBSDKShareLinkContent = FBSDKShareLinkContent()
+                    content.contentURL = NSURL(string: "http://www.snapsnap.com.sg/webdev/album")// + self.PostId)
+                    content.contentTitle = self.PostTitle.text
+                    content.contentDescription = self.PostHashtags.text
+                    content.imageURL = NSURL(string: (self.parentView as! HomeViewController).data.findEntry(self.PostId).media_url!)
+                    
+                    let button : FBSDKShareButton = FBSDKShareButton()
+                    button.shareContent = content
+                    button.frame = CGRectMake((UIScreen.mainScreen().bounds.width - 100) * 0.5, 50, 100, 25)
+                    button.hidden = true
+                    button.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
+                    
+                }
+            })
+            
+            let flagAction = UIAlertAction(title: "Flag as inappropriate", style: .Destructive, handler: {
+                (alert: UIAlertAction!) -> Void in
+                
+                if self.parentView is HomeViewController {
+                    
+                    var urlString = "http://0720backendapi15.snapsnap.com.sg/index.php/dphodto/action_flag_as_inappropriate/" + self.PostId
+                    let url = NSURL(string: urlString)
+                    var request = NSURLRequest(URL: url!)
+                    let queue: NSOperationQueue = NSOperationQueue.mainQueue()
+                    NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                        
+                        if error == nil {
+                            if (response as! NSHTTPURLResponse).statusCode == 200 {
+                                if data != nil {
+                                    var str = NSString(data: data, encoding: NSUTF8StringEncoding)
+                                    
+                                    if str == "completed" {
+                                        // Remove post from post data in code behind and refresh view
+                                        (self.parentView as! HomeViewController).data.removeEntry(self.PostId)
+                                        (self.parentView as! HomeViewController).HomeTableView.reloadData()
+                                        
+                                        var flagAlert = UIAlertController(title: "", message: "You have flagged the post as inappropriate.", preferredStyle: UIAlertControllerStyle.Alert)
+                                        flagAlert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Cancel, handler: nil))
+                                        (self.parentView as! HomeViewController).presentViewController(flagAlert, animated: true, completion: nil)
+                                    }
+                                }
+                            } else {
+                                println(response)
+                                // Insert action here for updating UI
+                            }
+                        } else {
+                            println(error)
+                            // Insert action here for updating UI
+                        }
+                    })
+                }
+            })
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {
+                (alert: UIAlertAction!) -> Void in
+            })
+            
+            optionMenu.addAction(shareAction)
+            optionMenu.addAction(flagAction)
+            optionMenu.addAction(cancelAction)
+            
+            parentView.presentViewController(optionMenu, animated: true, completion: nil)
+        }
     }
     
     @IBAction func ImageTouch(sender: AnyObject) {
-        if parentView is HomeViewController {
-            (parentView as! HomeViewController).selectedPostCellId = PostId
-            (parentView as! HomeViewController).selectedPostCell = self
-        }
         
-        parentView.performSegueWithIdentifier("ShowImageViewer", sender:self)
+        if PostId != nil {
+            if parentView is HomeViewController {
+                (parentView as! HomeViewController).selectedPostCellId = PostId
+                (parentView as! HomeViewController).selectedPostCell = self
+            }
+            
+            parentView.performSegueWithIdentifier("ShowImageViewer", sender:self)
+        }
     }
 }
